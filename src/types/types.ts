341cd585d2dab4aa0c29cccc725e0d6b7bf4e41e
tszhong0411 <lainelson411@gmail.com>
2022-08-20 16:02:17 +0800

    :fire: 移除沒有用的 type

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,24 +1,4 @@
-import React from 'react'
-
-import { defaultMeta } from '@/components/Layout/SEO'
-
-export type ChildrenType = {
-  children: React.ReactNode
-}
-
 export type storeType = {
   opened: boolean
   toggleOpened: (bool?: boolean) => void
 }
-
-export type FaviconType = {
-  rel: string
-  href: string
-  sizes?: string
-  type?: string
-}
-
-export type SeoProps = {
-  description?: string
-  templateTitle?: string
-} & Partial<typeof defaultMeta>
